e55f670baf6faab2330da95aa127fc0f7fe7eef2
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-04-06 15:00:34 +0700

    initial commit

---
+++ LatihanMemberClass/PlaneTicket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class PlaneTicket
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Cost { get; set; }
+        public string Currency { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin : {0}", Origin);
+            Console.WriteLine("Destination : {0}", Destination);
+            Console.WriteLine("Cost : {0} ({1})", Cost, Currency);
+            if(Currency == "USD")
+            {
+                Console.WriteLine("Convert to Rupiah : {0}", (Cost*15000));
+            }
+            else if(Currency == "IDR")
+            {
+                Console.WriteLine("Convert to Dollar : {0}", (Cost/15000));
+            }
+
+        }
+    }
+}
---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            // assign object ticket1
+            ticket1.Origin = "JOGJA";
+            ticket1.Destination = "PKU";
+            ticket1.Currency = "USD";
+            ticket1.Cost = 100;
+
+            // assign object ticket2
+            ticket2.Origin = "JKT";
+            ticket2.Destination = "PKU";
+            ticket2.Currency = "IDR";
+            ticket2.Cost = 750000;
+
+            // PRINT
+            ticket1.PrintSummary();
+            Console.WriteLine();
+            ticket2.PrintSummary();
+        }
+    }
+}
